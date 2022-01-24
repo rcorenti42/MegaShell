dd73082e38949cf9ef1a25d3d3ff10fc3db4596f
Richard CORENTIN <rcorenti@e2r1p14.clusters.42paris.fr>
2022-01-24 15:32:51 +0100

    add ft_strcat to libft

--- libft/ft_strcat.c
+++ libft/ft_strcat.c
@@ -1,21 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rcorenti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/20 18:43:12 by rcorenti          #+#    #+#             */
-/*   Updated: 2022/01/24 15:08:55 by rcorenti         ###   ########.fr       */
+/*   Created: 2022/01/24 15:08:08 by rcorenti          #+#    #+#             */
+/*   Updated: 2022/01/24 15:30:53 by rcorenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putendl_fd(char *str, int fd)
+char	*ft_strcat(char *dest, const char *src)
 {
-	if (!str)
-		return ;
-	write(fd, str, ft_strlen(str));
-	write(fd, "\n", 1);
+	char	*ret;
+	int		i;
+	int		j;
+
+	ret = (char *)malloc(sizeof(char) * (ft_strlen(dest)
+				+ ft_strlen((char *)src) + 1));
+	i = -1;
+	if (dest)
+		while (dest[++i])
+			ret[i] = dest[i];
+	j = -1;
+	if (src)
+	{
+		while (src[++j])
+		{
+			ret[i] = src[j];
+			i++;
+		}
+	}
+	ret[i] = '\0';
+	return (ret);
 }
