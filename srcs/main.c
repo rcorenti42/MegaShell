9dd08263023b8d8214bd1d22daf0e4b814a5239e
Richard CORENTIN <rcorenti@e2r1p14.clusters.42paris.fr>
2022-01-24 12:42:46 +0100

    base

---
+++ srcs/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcorenti <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/24 12:28:55 by rcorenti          #+#    #+#             */
+/*   Updated: 2022/01/24 12:41:13 by rcorenti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_shell	*shell;
+
+	(void)argc;
+	(void)argv;
+	init_shell(&shell);
+	while (!shell.exit)
+	{
+	}
+	return (shell.ret);
+}
